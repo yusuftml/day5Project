a7654dd1fdc56309396c2e2b9bec6b4edd9cf3ec
yusuftml <yusuftemel355@gmail.com>
2020-10-13 19:32:20 +0200

    added comment same line

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello branch");
-        System.out.println("Hello branch");
+        System.out.println("Hello branch");System.out.println("Add one more line");
+
     }
 }
