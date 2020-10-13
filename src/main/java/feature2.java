8c8369e435864305a2a9a44b20a141768f58db98
yusuftml <yusuftemel355@gmail.com>
2020-10-13 18:46:50 +0200

    feature2 second commit

--- src/main/java/feature2.java
+++ src/main/java/feature2.java
@@ -1,5 +1,7 @@
 public class feature2 {
     public static void main(String[] args) {
         System.out.println("Working on feature2");
+        System.out.println("Working on feature2 again");
+
     }
 }
